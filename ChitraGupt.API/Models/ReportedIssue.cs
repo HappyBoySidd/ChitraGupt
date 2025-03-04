e6321e0fd6a124984667e283c1540e642d90e3f4
Siddharth Mukherjee <68189505+HappyBoySidd@users.noreply.github.com>
2025-03-04 10:57:19 +0530

    added property for L0 value

--- ChitraGupt.API/Models/ReportedIssue.cs
+++ ChitraGupt.API/Models/ReportedIssue.cs
@@ -5,6 +5,7 @@
         public long DefectId { get; set; }
         public required string Description { get; set; }
         public string? ShortDescription { get; set; }
+        public string? L0 { get; set; }
         public string? L1 { get; set; }
         public string? L2 { get; set; }
         public string? L3 { get; set; }
