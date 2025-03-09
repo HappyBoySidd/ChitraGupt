682572cb78eefd1ccba59fcabeb9ebc6dc03d110
Siddharth Mukherjee <68189505+HappyBoySidd@users.noreply.github.com>
2025-03-09 23:27:40 +0530

    Added translation controller

---
+++ ChitraGupt.API/Controllers/TranslationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ChitraGupt.API.Interfaces;
+
+namespace Translate.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TranslationController : ControllerBase
+    {
+        private readonly ITranslate _translationService;
+
+        public TranslationController(ITranslate translationService)
+        {
+            _translationService = translationService;
+        }
+
+        [HttpGet]
+        public IActionResult TranslateText(string strText)
+        {
+            if(string.IsNullOrWhiteSpace(strText))
+                return StatusCode(StatusCodes.Status406NotAcceptable, "Invalid Input");
+            var strTranslatedText = _translationService.TranslateText(strText).Result;
+            if (string.IsNullOrWhiteSpace(strTranslatedText))
+                return StatusCode(StatusCodes.Status500InternalServerError, "Unable to translate");
+            return Ok(strTranslatedText);
+        }
+
+    }
+}
